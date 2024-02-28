1eef79951d1d936c1a097e5927f99c077a180077
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-02-28 22:49:05 +0100

    jte

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -2,8 +2,7 @@ void call() {
     agent { docker { image 'maven:3.3.3' } }
     stage('logVersion') {
       steps {
-        sh 'mvn --version'
-        sh 'mvn clean install'
+        sh 'mvn --version && mvn clean install'
       }
     }
 }
