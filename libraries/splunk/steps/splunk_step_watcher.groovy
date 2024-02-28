671264f7748737cb6800a51afd2c1af3b6bc2eac
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-02-28 14:18:21 +0100

    jte

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,6 +3,7 @@ void before() {
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step" 
 }
 
+//restriction for executing only after a specific step
 @AfterStep({ hookContext.step in config.afterSteps })
 void after() {
     println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step" 
