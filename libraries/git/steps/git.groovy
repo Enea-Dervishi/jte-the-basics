3cc7e2fabcb350f30c523b0f4bce9e1950aec23a
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-03-04 17:13:11 +0100

    jte

--- libraries/git/steps/git.groovy
+++ libraries/git/steps/git.groovy
@@ -1,3 +1,11 @@
-checkout scmGit(
-    branches: [[name: 'master']],
-    userRemoteConfigs: [[url: 'https://github.com/jenkinsci/kubernetes-plugin']])
+void call() {
+    node {
+        stage('checkout') {
+            steps {
+                script {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/jenkinsci/kubernetes-plugin.git']]])
+                }
+            }
+        }
+    }
+}
