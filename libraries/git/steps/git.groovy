7dc6c23f87449283d44650b72498fe9269a33e05
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-03-04 15:14:15 +0100

    jte

--- libraries/git/steps/git.groovy
+++ libraries/git/steps/git.groovy
@@ -1,20 +1,10 @@
-/*
-    takes an arraylist of files to pass to git add 
-*/
-void add(ArrayList files) {
-    println "git add ${files.join(" ")}"
-}
-
-/*
-    takes a string commit message to pass to git commit 
-*/
-void commit(String message) {
-    println "git commit -m ${message}" 
-}
-
-/*
-    performs the git push
-*/
-void push() {
-    println "git push" 
+void call(Map args) {
+    node {
+        stage('Checkout'){
+            actions = [
+                "checkout": { branch ->
+                    sh "git checkout 'https://github.com/jenkinsci/kubernetes-plugin.git'" }
+            ]
+        }
+    }
 }
--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,12 +1,16 @@
 void call() {
-            stage('mavenbuild') {
-              agent { docker 'maven:3.9-eclipse-temurin-21' }
-                steps{
-                    ('build') {
+    podTemplate(containers: [
+        containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21', ttyEnabled: true, command: 'cat')
+    ]) {
+        node(POD_LABEL) {
+            stage('Build a Maven project'){
+                container('maven') {
+                    stage('Build a Maven project') {
                         sh 'mvn -B clean install'
                     }
                 }
             }
         }
-    
+    }
+}
 
