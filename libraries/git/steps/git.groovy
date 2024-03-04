cf16a7ec18e958bfc93d765d7c53bedf8109af22
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-03-04 16:36:03 +0100

    jte

--- libraries/git/steps/git.groovy
+++ libraries/git/steps/git.groovy
@@ -2,9 +2,9 @@ void call() {
     node {
             stage('Checkout') {
                 steps {
-                    sh 'git checkout https://github.com/jenkinsci/kubernetes-plugin.git'
+                    sh 'git clone https://github.com/jenkinsci/kubernetes-plugin.git'
                 }
             }
         }
     }
-}
+
