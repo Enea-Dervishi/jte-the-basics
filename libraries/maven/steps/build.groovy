c1a79d6cd11ad79cdab20213a16aa88e8757680a
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-03-04 10:13:04 +0100

    jte

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,7 +1,18 @@
 void call() {
-    stage('build') {
-      docker.image("maven:3.3.3").inside{
-        sh 'mvn --version && mvn clean install'
-      }
+    podTemplate(containers: [
+        containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21', ttyEnabled: true, command: 'cat')
+    ]) {
+        node {
+            label 'build-maven'
+            stage('mavenbuild') {
+                steps{
+                container('maven') {
+                    ('build') {
+                        sh 'mvn -B clean install'
+                    }
+                }
+                }
+            }
+        }
     }
 }
