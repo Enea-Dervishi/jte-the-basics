3b6cf3b55257e1dc3dce18e626ad574faeff7547
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-03-04 16:34:31 +0100

    jte

--- libraries/git/steps/git.groovy
+++ libraries/git/steps/git.groovy
@@ -1,11 +1,8 @@
 void call() {
     node {
-        podTemplate(containers: [
-            containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21', ttyEnabled: true, command: 'cat')
-        ]) {
             stage('Checkout') {
-                container('maven') {
-                    sh 'git clone https://github.com/jenkinsci/kubernetes-plugin.git'
+                steps {
+                    sh 'git checkout https://github.com/jenkinsci/kubernetes-plugin.git'
                 }
             }
         }
