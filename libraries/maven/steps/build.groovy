f47f87286e0069ebea88a03269fcff358a88b769
Enea-Dervishi <eneaedervishi14@gmail.com>
2024-03-05 15:50:37 +0100

    jte

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,21 +1,31 @@
 void call() {
-  podTemplate(containers: [
-        containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21', ttyEnabled: true, command: 'cat')
-    ]) {
-        agent {
-      node(POD_LABEL) {
-        label 'master'
-        stages {
-          stage('Build a Maven project') {
-            steps{
+    podTemplate(
+        containers: [
+            containerTemplate(name: 'maven', image: 'maven:3.9-eclipse-temurin-21', ttyEnabled: true, command: 'cat')
+        ]
+    ) {
+        node(POD_LABEL) {
+             stage('Get a Maven project') {
+            git 'https://github.com/jenkinsci/kubernetes-plugin.git'
             container('maven') {
-              sh 'mvn -B clean install'
+                stage('Build a Maven project') {
+                    sh 'mvn -B -ntp clean install'
+                }
             }
-            }
-          }
         }
-      }
+
+        stage('Get a Golang project') {
+            git url: 'https://github.com/hashicorp/terraform.git', branch: 'main'
+            container('golang') {
+                stage('Build a Go project') {
+                    sh '''
+                    mkdir -p /go/src/github.com/hashicorp
+                    ln -s `pwd` /go/src/github.com/hashicorp/terraform
+                    cd /go/src/github.com/hashicorp/terraform && make
+                    '''
+                }
+            }
+        }
         }
     }
 }
-
